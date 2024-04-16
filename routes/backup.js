d70fbca10ac1777c55f3d596fd5e81b19ff95e10
SAURGUP <saba.urgup@mercedes-benz.com>
2024-04-16 14:22:10 +0300

    fix: Email sending problem solved

--- routes/backup.js
+++ routes/backup.js
@@ -18,97 +18,49 @@ const transporter = nodemailer.createTransport({
 
 router.post('/mail', async (req, res) => {
   try {
-    const filePath = await saveStudentsToJsonFile();
-    if (filePath) {
-      await sendEmail(filePath);
-    } else {
-      res.status(500).json({ error: 'Öğrenci listesi dosyası oluşturulamadı' });
-    }
+    await sendEmail();
+    res.json({ message: 'E-posta başarıyla gönderildi' });
   } catch (error) {
     console.error('E-posta gönderme hatası:', error);
     res.status(500).json({ error: 'E-posta gönderme hatası' });
   }
 });
 
-// router.post('/mail', async (req, res) => {
-//   try {
-//     const { name, email, message } = req.body;
-//     const mailOptions = {
-//       from: process.env.MAIL_USERNAME,
-//       to: email,
-//       subject: `Merhaba ${name},`,
-//       text: message
-//     };
-//     transporter.sendMail(mailOptions, (error, info) => {
-//       if (error) {
-//         console.error('E-posta gönderme hatası:', error);
-//         res.status(500).json({ error: 'E-posta gönderme hatası' });
-//       } else {
-//         console.log('E-posta gönderildi:', info.response);
-//         res.json({ message: 'E-posta başarıyla gönderildi' });
-//       }
-//     });
-//   } catch (error) {
-
-//   }
-// });
-
 function calculateBackupTimes(period) {
-  const backupTime = '0 9 * * 1'; // Dakika, saat, gün, ay, haftanın günü (0: Pazar, 1: Pazartesi, ..., 6: Cumartesi)
+  const backupTime = '15 09 * * 3'; // Dakika, saat, gün, ay, haftanın günü (0: Pazar, 1: Pazartesi, ..., 6: Cumartesi)
   return backupTime;
 }
 
 const backupTime = calculateBackupTimes(PERIOD);
 
-async function saveStudentsToJsonFile() {
+async function sendEmail() {
   try {
     const { rows: students } = await pool.query('SELECT * FROM students');
-    const jsonData = JSON.stringify(students, null, 2); // JSON verisini 2 boşluk ile girintiler
+    const jsonData = JSON.stringify(students, null, 2);
     const filePath = './students.json';
-    fs.writeFileSync(filePath, jsonData);
-    return filePath;
-  } catch (error) {
-    console.error('Öğrenci listesini dosyaya yazma hatası:', error);
-    return null;
-  }
-}
-
-async function sendEmail(filePath) {
-  try {
-      const { name, email } = req.body;
-      const mailOptions = {
-        from: process.env.MAIL_USERNAME,
-        to: email,
-        subject: `Merhaba ${name} Haftalık Rapor,`,
-        text: 'Haftalık yedekleme raporu ektedir.',
-        attachments: [
-          {
-            filename: 'students.json',
-            path: filePath
-          }
-        ]
-      };
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error('E-posta gönderme hatası:', error);
-          res.status(500).json({ error: 'E-posta gönderme hatası' });
-        } else {
-          console.log('E-posta gönderildi:', info.response);
-          res.json({ message: 'E-posta başarıyla gönderildi' });
+    const mailOptions = {
+      from: process.env.MAIL_USERNAME,
+      to: process.env.MAIL_TO,
+      subject: `Haftalık Rapor`,
+      text: `\n Haftalık yedekleme raporu ektedir: \n  ${jsonData}`,
+      attachments: [
+        {
+          filename: 'students.json',
+          path: filePath
         }
-      });
+      ]
+    };
+    await transporter.sendMail(mailOptions);
+    console.log('E-posta gönderildi');
   } catch (error) {
     console.error('E-posta gönderme hatası:', error);
-    res.status(500).json({ error: 'E-posta gönderme hatası' });
+    throw error;
   }
 }
 
 cron.schedule(backupTime, async () => {
   try {
-    const filePath = await saveStudentsToJsonFile();
-    if (filePath) {
-      await sendEmail(filePath);
-    }
+    await sendEmail();
   } catch (error) {
     console.error('Haftalık rapor oluşturma hatası:', error);
   }
